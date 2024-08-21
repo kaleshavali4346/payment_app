954adde6cfc6da7553d4f750f0e56490dd473b76
kaleshavali4346 <shaikkalesha.vali2021@vitstudent.ac.in>
2024-08-21 23:36:51 +0530

    added transfer end point in account route  using mongo db transactions

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { authMiddleware } = require("../middleware");
-const { Account } = require("../db");
+const { Account, User } = require("../db");
+const { mongo } = require("mongoose");
 
 const router = express.Router();
 
@@ -11,6 +13,37 @@ router.get("/balance",authMiddleware,async (req,res)=>{
     res.json({
         balance:account.balance
     })
+});
+//use transactions in db
+router.post("/transfer",authMiddleware,async (req,res)=>{
+   const session = await mongoose.startSession();
+
+   session.startTransaction();
+   const {amount , to} = req.body;
+   const account = await Account.findOne({userId:req.userId}).session(session);
+
+   if(!account || account.balance<amount){
+    await session.abortTransaction();
+    return res.status(400).json({
+        message:"Insufficient balance"
+    });
+    }
+
+   const toAccount = await Account.findOne({userId:to}).session(session);
+   if(!toAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message:"Account not found"
+        })
+    }
+    await Account.updateOne({userId:req.userId},{$inc:{balance:-amount}}).session(session);
+    await Account.updateOne({userId:to},{$inc:{balance:amount}}).session(session);
+
+    await session.commitTransaction();
+
+    res.json({
+        message:"Transfer successful"
+    });
 });
 
 module.exports=router;
